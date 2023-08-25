230a3d2599e848d1080f25f36e05ec58b29f3606
santosh73 <143166435+santosh73@users.noreply.github.com>
2023-08-25 12:42:41 +0530

    Add files via upload

---
+++ santosh_assignment.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+class ICommunicationManager{
+
+    void shareContent(string imageLink);
+
+}
+
+class ImageGallery {
+	string images[5] = { "one.jpg","two.jpg","three.jpg","four.jpg","fiv.jpg"};
+public:void share(int index, ICommunicationManager* comManager) {
+
+    comManager->shareContent(images[index]);
+
+}
+};
+
+class BluetoothManager : public ICommunicationManager{
+public:
+	void searchDevices() {}
+    void pairdevice(){}
+	void sendContent() {}
+	void turnOn() {}
+	void turnOff(){}
+    void shareContent(string imageLink){}
+};
+
+class WhatsUpManager : public ICommunicationManager {
+	void turnOn() {}
+	void turnOff() {}
+	void shareContent() {}
+	void joinGroup() {}
+	void addMember(){}
+	void createGroup() {}
+    void shareContent(string imageLink){}
+
+};
+
+class MailManager : public ICommunicationManager{
+
+	void pullNewMails() {}
+	void senMails(){}
+	void composeEmail(){}
+	void addAttachement() {}
+    void shareContent(string imageLink){}
+};
+
+int main() {
+
+	MailManager* _mailManager = new  MailManager();
+	BluetoothManager* _blueToothManager = new  BluetoothManager();
+	WhatsUpManager* _whatsAppManager = new  WhatsUpManager();
+	
+	ImageGallery* imageMgmt = new  ImageGallery();
+	imageMgmt->share(0, _mailManager);
+	imageMgmt->share(0, _blueToothManager);
+    imageMgmt->share(0, _whatsAppManager);
+}
